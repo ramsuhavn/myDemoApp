4ecc2b9880fc283d3abde5f002a06cccfc4d922e
ramsuhavanm <rp21075@gmail.com>
2021-04-26 16:34:51 +0530

    new chnages added to the app components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mydemoappAnjular Ramsuhavan Patel!!!!! test for personal';
+  title = 'mydemoappAnjular Ramsuhavan Patel!!!!! test for personal  I added for all user';
 }

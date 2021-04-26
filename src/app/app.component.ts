eabda7b52b838f82ce4f9124bec25dad07c20e4a
ramsuhavanm <rp21075@gmail.com>
2021-04-26 16:54:01 +0530

    chnages is added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mydemoappAnjular Ramsuhavan Patel!!!!! Feature2 will create feature3';
+  my = 'my test1 file ';
 }
